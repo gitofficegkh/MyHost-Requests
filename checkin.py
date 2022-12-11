fe8f750603ad7548128835a8ea4716904a854fdd
gitofficegkh <119872727+gitofficegkh@users.noreply.github.com>
2022-12-11 23:51:42 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,8 +1,9 @@
 # encoding=utf8
 import requests
 import json
+import re
 def connect(cookie_string):
-    # 灰狼云续费url
+    url1 = "https://www.yun316.net/host/panel/3342"
     url2 = "https://www.yun316.net/data/host.php?act=renew&id=3342&planid=3342&m=1"
     my_headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.42',
@@ -13,13 +14,23 @@ def connect(cookie_string):
     if response.status_code != 200: 
       checkin_code = 0
       message = f"{message}Status:error\n"
-      message = f"{message}Checkin:Login fails, please check your cookie.\n"
+      message = f"{message}Reasons:Login fails, please check your cookie.\n"
       return checkin_code, message
-    # print(response.text)  # response.text和浏览器返回数据相同说明post数据成功
     checkin_code = 1
     resp = json.loads(response.text)
     res = resp["sta"]
-    message = f"{message}Status:{res}\n"
-    res = resp["msg"]
-    message = f"{message}Checkin:{res}\n"
+    if "error" in res:
+        message = f"{message}Status:{res}\n"
+        res = resp["msg"]
+        message = f"{message}Reasons:{res}\n"
+    eles:
+        res = resp["msg"]
+        message = f"{message}Status:{res}\n"
+        response = requests.post(url1, headers=my_headers)
+        obj = re.compile(r"<tr><td>到期时间</td><td>(?P<time>.*?)</td>")
+        it = obj.finditer(response.text)
+        res = "未知"
+        for i in it:
+            res = i.group("time")
+        message = f"{message}到期时间:{res}\n"
     return checkin_code, message
