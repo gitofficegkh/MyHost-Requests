715760893f48b71f51ac3241fbe8be3e2af037b1
gitofficegkh <119872727+gitofficegkh@users.noreply.github.com>
2022-12-15 00:50:09 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -24,6 +24,7 @@ def connect(cookie_string):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.42',
         'cookie': cookie_string,
         'referer': 'https://www.yun316.net/host/panel/3342',
+        'sec-ch-ua': 'Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108'
         'accept-encoding': 'gzip, deflate, br',
     }
     response = requests.get(url2, headers=my_headers2)
