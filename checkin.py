2c5e00e633cfd1333afc137d5d8414d0049ce04d
gitofficegkh <119872727+gitofficegkh@users.noreply.github.com>
2022-12-18 23:15:15 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -37,7 +37,19 @@ def connect(cookie_string):
         'upgrade-insecure-requests': '1',
     }
     time.sleep(6)
-    response = requests.post(url2, headers=my_headers2, verify=False)
+    # response = requests.post(url2, headers=my_headers2, verify=False)
+    session = requests.session()
+    #账号密码存放处
+    data = {
+        'email': 'gukeheng@163.com',
+        'password': '85621463'
+        'remember': 'on'
+    }
+    # 登录
+    url = "https://www.yun316.net/data/user.php?act=login"
+    session.post(url,data=data)
+    # 签到
+    response = session.post(url2)
     message = str()
     if response.status_code != 200: 
         checkin_code = 0
